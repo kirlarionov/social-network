5156762f11990d0f5a1c8638db0666d3c3af449e
Kirill <kirkeriil@gmail.com>
2022-11-20 16:47:37 +0200

    fixes routes and added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
-import { Routes, Route } from 'react-router-dom'
-import { styled } from '@mui/material/styles'
-import { Box, CssBaseline } from '@mui/material'
+import { Routes, Route } from "react-router-dom"
+import { styled } from "@mui/material/styles"
+import { Box, CssBaseline } from "@mui/material"
 import AppHeader from "./components/AppHeader"
 import LeftSidebar from "./components/LeftSidebar"
 import MainContentHeader from "./components/MainContentHeader"
@@ -9,36 +9,36 @@ import PageLinks from "./components/PageLinks"
 import Home from "./pages/Home"
 import Articles from "./pages/Articles"
 import Team from "./pages/Team"
-import { ArticleProvider } from './context-articles'
-import { MessagesProvider } from './context-messages'
+import { ArticleProvider } from "./context-articles"
+import { MessagesProvider } from "./context-messages"
 
 const DRAWER_WIDTH = 280
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
    ({ theme, open }) => ({
       flexGrow: 1,
       padding: theme.spacing(3),
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
          easing: theme.transitions.easing.sharp,
          duration: theme.transitions.duration.leavingScreen,
       }),
       marginLeft: `-${DRAWER_WIDTH}px`,
       ...(open && {
-         transition: theme.transitions.create('margin', {
+         transition: theme.transitions.create("margin", {
             easing: theme.transitions.easing.easeOut,
             duration: theme.transitions.duration.enteringScreen,
          }),
          marginLeft: 0,
       }),
-   }),
+   })
 )
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-   display: 'flex',
-   alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+   display: "flex",
+   alignItems: "center",
    padding: theme.spacing(0, 1),
    ...theme.mixins.toolbar,
-   justifyContent: 'flex-end',
+   justifyContent: "flex-end",
 }))
 
 const App = () => {
@@ -48,7 +48,7 @@ const App = () => {
    return (
       <ArticleProvider>
          <MessagesProvider>
-            <Box sx={{ display: 'flex' }}>
+            <Box sx={{ display: "flex" }}>
                <CssBaseline />
 
                <AppHeader open={open} handleDrawerOpen={handleDrawerOpen} />
@@ -58,15 +58,17 @@ const App = () => {
                   <MainContentHeader />
                   <PageLinks />
                   <Routes>
-                     <Route path="/" element={<Home />} />
-                     <Route path="/articles" element={<Articles />} />
-                     <Route path="/team" element={<Team />} />
+                     <Route path="/social-network/" element={<Home />} />
+                     <Route
+                        path="/social-network/articles"
+                        element={<Articles />}
+                     />
+                     <Route path="/social-network/team" element={<Team />} />
                   </Routes>
                </Main>
-
             </Box>
          </MessagesProvider>
-      </ArticleProvider >
+      </ArticleProvider>
    )
 }
 
--- src/components/PageLinks.js
+++ src/components/PageLinks.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { styled } from '@mui/material/styles'
-import { Box } from '@mui/material'
+import React from "react"
+import { NavLink } from "react-router-dom"
+import { styled } from "@mui/material/styles"
+import { Box } from "@mui/material"
 
 const PageLink = styled(NavLink)`
    font-size: 20px;
@@ -11,13 +11,13 @@ const PageLink = styled(NavLink)`
    border: 1px solid #c5e4ea;
    border-bottom: 0;
    border-radius: 15px 15px 0 0;
-      &:hover {
-         background-color: #e4f8f9;
-         transition: all .3s;
-      }
-      &.active {
-         background-color: #e4f8f9;
-      }
+   &:hover {
+      background-color: #e4f8f9;
+      transition: all 0.3s;
+   }
+   &.active {
+      background-color: #e4f8f9;
+   }
 `
 const Line = styled(Box)`
    width: 100%;
@@ -27,14 +27,14 @@ const Line = styled(Box)`
 `
 
 const PageLinks = () => (
-      <>
-         <Box>
-            <PageLink to='/' >Home</PageLink>
-            <PageLink to='/articles'>Articles</PageLink>
-            <PageLink to='/team'>Team</PageLink>
-         </Box>
-         <Line />
-      </>
-   )
+   <>
+      <Box>
+         <PageLink to="/social-network">Home</PageLink>
+         <PageLink to="/social-network/articles">Articles</PageLink>
+         <PageLink to="/social-network/team">Team</PageLink>
+      </Box>
+      <Line />
+   </>
+)
 
 export default PageLinks
